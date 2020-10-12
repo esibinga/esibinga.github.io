8d08d650afc4c0ac6de8c3bef9d9c6a260d75d61
Eva Sibinga <evasibinga@MacBook-Pro-5.local>
2020-10-12 11:47:49 -0400

    setup

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import barba from './@barba/core';
+// import barba from './@barba/core';
 
-barba.init({
-    transitions: [{
-        name: 'opacity-transition',
-        leave(data) {
-            return gsap.to(data.current.container, {
-                opacity: 0
-            });
-        },
-        enter(data) {
-            return gsap.from(data.next.container, {
-                opacity: 0
-            });
-        }
-    }]
-});
+// barba.init({
+//     transitions: [{
+//         name: 'opacity-transition',
+//         leave(data) {
+//             return gsap.to(data.current.container, {
+//                 opacity: 0
+//             });
+//         },
+//         enter(data) {
+//             return gsap.from(data.next.container, {
+//                 opacity: 0
+//             });
+//         }
+//     }]
+// });
 
